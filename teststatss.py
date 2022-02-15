bbbd7efcf2284b7bbb61e4f4990cb2d9e6b96f7c
farshadsafari <99046397+farshadsafari@users.noreply.github.com>
2022-02-16 01:25:47 +0330

    Update teststatss.py

--- teststatss.py
+++ teststatss.py
@@ -85,75 +85,7 @@ def killedby(c,m):
 **●┛{mqdar5}**  |  {fard5}
 """)
 
-safe = []
 
-with app:
-    me = app.get_me().id
-
-helpmsg = """**If you do not want to block the message, put the message in PV
-/n safeyou /n
-If someone was safe and you wanted to get out of safe
-/n dsafeyou /n
-Click on his message
-
-People who are not safe will be automatically blocked if they send a sticker, link or ID, and the message sent in PV will be saved in Siomessage by mentioning the sender.**"""
-
-@app.on_message(filters.regex("my helpp") & filters.private)
-def help_msg(client,message):
-    message.reply_text(helpmsg)
-
-@app.on_message(filters.text & filters.private)
-def anti_attack(c, m):
-    if m.text == 'safeyou':
-        user = m.reply_to_message.from_user
-        safe.append(user.id)
-        app.send_message(m.chat.id,f"{user.mention}**Added to safe list✔**")
-
-    if m.text == 'dsafeyou':
-        user = m.reply_to_message.from_user
-        safe.remove(user.id)
-        app.send_message(m.chat.id,f"{user.mention}**Removed from safe list🗑✔**")
-
-    if "t.me" in m.text and m.from_user.id not in safe:
-        app.send_message(me, f"[A link was received from him and was blocked](tg://user?id={m.from_user.id})")
-        app.send_message(me, "**Link sent:**")
-        app.forward_messages(me, m.chat.id, m.message_id)
-        app.block_user(user_id=m.chat.id)
-        app.delete_messages(m.chat.id, m.message_id)
-
-    if "@" in m.text and m.from_user.id not in safe:
-        app.send_message(me, f"[An ID was received from him and it was blocked](tg://user?id={m.from_user.id})")
-        app.send_message(me, "**Posted ID:**")
-        app.forward_messages(me, m.chat.id, m.message_id)
-        app.block_user(user_id=m.chat.id)
-        app.delete_messages(m.chat.id, m.message_id)
-
-@app.on_message(filters.sticker & filters.private)
-def st_attack(c,m):
-    if m.chat.id not in safe:
-        app.send_message(me, f"[A sticker was received from him and it was blocked](tg://user?id={m.from_user.id})")
-        app.send_message(me, "**Sticker sent:**")
-        app.forward_messages(me, m.chat.id, m.message_id)
-        app.block_user(user_id=m.chat.id)
-        app.delete_messages(m.chat.id, m.message_id)
-
-@app.on_message(filters.caption & filters.private)
-def cap_attack(c, m):
-    if "t.me" in m.caption and m.from_user.id not in safe:
-        app.send_message(me, f"[A link was received from him and was blocked](tg://user?id={m.from_user.id})")
-        app.send_message(me, "**Link sent:**")
-        app.forward_messages(me, m.chat.id, m.message_id)
-        app.block_user(user_id=m.chat.id)
-        app.delete_messages(m.chat.id, m.message_id)
-
-    if "@" in m.caption and m.from_user.id not in safe:
-        app.send_message(me, f"[An ID was received from him and it was blocked](tg://user?id={m.from_user.id})")
-        app.send_message(me, "**Posted ID:**")
-        app.forward_messages(me, m.chat.id, m.message_id)
-        app.block_user(user_id=m.chat.id)
-        app.delete_messages(m.chat.id, m.message_id)
-        
-        
 print("@farrshad1")
 
 app.run()
