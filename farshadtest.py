4bc1eb160f55fccefd9d1f3b8d7c2460910cbb45
farshadsafari <99046397+farshadsafari@users.noreply.github.com>
2022-02-16 20:43:04 +0330

    Update farshadtest.py

--- farshadtest.py
+++ farshadtest.py
@@ -101,9 +101,8 @@ active = [5296357997,2113150493,205092371,198626752,175844556,742956373]
 
 @app.on_message (filters.text & filters.group & ~filters.edited)
 def heln(c, m):
-    if "test" in m.text and m.from_user.id in active:
-        app.send_message(m.chat.id, "**test**",)
-    global nextt       
+    global nextt 
+    app.send_message(m.chat.id, "test",)
     if m.text == "setlinkferisafari":
         nextt = m.reply_to_message.text
         app.send_message(m.chat.id,  " پیام تنظیم شده {setlinkferi}")
