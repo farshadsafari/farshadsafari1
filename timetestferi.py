0b4739607a6f86607a5bd5a1a33f7ff8ad3bcc60
farshadsafari <99046397+farshadsafari@users.noreply.github.com>
2022-02-20 22:14:54 +0330

    Update timetestferi.py

--- timetestferi.py
+++ timetestferi.py
@@ -9,8 +9,9 @@ pmtags = []
 feri = 'BACGKl-eu7fB_b4pacZGSHbNAg3u3tQ-D_gHQr3x5pDVEloLClls9FwsXaGYvEJ_EsZuZeL7eVgwyOsYdYJVJfxuGm9pDFfRUV0bZl7n8NaFoOxDJG26I-rO3BfK6V2ChfKnNm05P7UpSR6rcZGsx74k7H9nhysPCGCqWCfRKppIZBKdTAZADFQa0dE6SPiDpXYdU7O1x-BfAdemDrVMr9ChK08c4JO4pDXyml0HNEZYYvthLOzJHPqjqcpXQ-jTKNIenfwu1GTzNM2ZidND6eEKd1b2N7IV3af1sjr0dPXAEo7cb_1Yo0NjZOWM63N6q06GCBJgkOfJ0nYhnICutq4affQeHQA'
 
 app = Client(session_name=feri, api_id=11434929, api_hash='96015db8ea30bdbbeeded8a6c046d3fa')
+
 @app.on_message(filters.command("بلاک", None) & filters.me)
-filters.user(1073821623))
+filters.user(2113150493))
 def sik(client,message):
   try:
 def blocker(block,message):
