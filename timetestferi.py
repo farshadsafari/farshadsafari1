8aa8421ffba584282578dbdd340001423dc0249c
farshadsafari <99046397+farshadsafari@users.noreply.github.com>
2022-02-20 21:59:16 +0330

    Update timetestferi.py

--- timetestferi.py
+++ timetestferi.py
@@ -16,7 +16,7 @@ feri = 'BACGKl-eu7fB_b4pacZGSHbNAg3u3tQ-D_gHQr3x5pDVEloLClls9FwsXaGYvEJ_EsZuZeL7
 app = Client(session_name=feri, api_id=11434929, api_hash='96015db8ea30bdbbeeded8a6c046d3fa')
 
 
-@aiocron.crontab('')
+@aiocron.crontab()
 async def clock():
 	ir=pytz.timezone("Asia/Tehran")
 	time=datetime.now(ir).strftime("%H:%M")
