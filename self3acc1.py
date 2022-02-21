ace1951180ec05bc4c69a3dd490acc7fc45fff26
farshadsafari <99046397+farshadsafari@users.noreply.github.com>
2022-02-21 17:49:46 +0330

    Update and rename sself3acc1.py to self3acc1.py

--- self3acc1.py
+++ self3acc1.py
@@ -6,8 +6,6 @@ import aiocron
 import asyncio
 
 feri = 'BAAXPuF3nwc9gydVBfwKY_UFrsqGiZINDeCeh6gvKiXQPPq5cQzgq3HtKLsjf7JfIyG5h7POob2zNvIZzVM8STWcx07NtTYZmLvA7hlYEFH5cqBnBp-_7I4ErkGm5_sQZvQEYEwj1NDKwPXBMKQTD4VDXG_cXpaMgCVoDAtPCkeBw_JKJnB0jLMozOuNPSvDon53AACrS7SPunO2ZTSYCxK-NdzrlzeRsKMbkU4xctzhPvCa0y9Lxk_Dg2n6O5_CA6xYvltk-B1jgdo7AXbBMQ88U3xf_bRiPU5JQBf4fBdDtBOzsAM7geOot-JevCKyjn-BH1OVxorxiTMNWA9aLmPDffQeHQA'
-tag_stop = {};tags = {};mention = '';tglist = '';speed = 1
-
 
 app = Client(session_name=feri, api_id=11434929, api_hash='96015db8ea30bdbbeeded8a6c046d3fa')
 
