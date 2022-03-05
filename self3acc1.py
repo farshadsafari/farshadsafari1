b8f23a17748275c9559ba724663cbadab819e02e
farshadsafari <99046397+farshadsafari@users.noreply.github.com>
2022-03-05 17:02:21 +0330

    Update self3acc1.py

--- self3acc1.py
+++ self3acc1.py
@@ -9,13 +9,45 @@ feri = 'BAA9WLj4NC92s0BRoKxVYHYOJYqqtxJyfNwMxSwKkZDOF-aGD7fSw7RngKE1cD6oR1kMO9Ai
 app = Client(session_name=feri, api_id=11434929, api_hash='96015db8ea30bdbbeeded8a6c046d3fa')
 
 
-@app.on_message(filters.me & filters.text & filters.command(['joingroup']) & filters.reply)
-def join(client, message):
+wlc_info = {}
+wlc_heh = {}
+
+@app.on_message(filters.text & filters.me & filters.regex('Setwelcome'))
+def setwlc(client,message):
+    global wlc_info,wlc_heh
+    chat_id = message.chat.id
+    if message.reply_to_message:
+        wlc_heh[message.chat.id] = True
+        wlc_info[chat_id] = message.reply_to_message.text
+        message.reply_text("**wlc seted✅**")
+
+@app.on_message(filters.new_chat_members)
+def wlc(client,message):
+    chat_id = message.chat.id
     try:
-        join = message.reply_to_message.text
-        client.join_chat(join)
-        message.edit_text("• Joined!")
-    except:
-        pass
+        if wlc_heh[message.chat.id]:
+            try:
+                message.reply_text(wlc_info[chat_id])
+            except KeyError:
+                return
+    except KeyError:
+        return
+@app.on_message(filters.text & filters.me & filters.regex('Offwelcome'))
+def wlcof(clientt,message):
+    global wlc_heh
+    wlc_heh[message.chat.id] = False 
+ 
+@app.on_message(filters.text & filters.me & filters.regex('Block'))
+def block_users(client,message):
+    user = message.reply_to_message.from_user.id
+    app.block_user(user)
+    message.reply_text('**User block✅**')
+
+
+@app.on_message(filters.text & filters.me & filters.regex('Unblock'))
+def unblock_user(client,message):
+    user = message.reply_to_message.from_user.id
+    app.unblock_user(user)
+    message.reply_text('**User unblock✅**')
     
 app.run()
