1f35707f861761e7310d2f5bb33b0d07e537eaad
farshadsafari <99046397+farshadsafari@users.noreply.github.com>
2022-03-05 16:30:23 +0330

    Update self3acc1.py

--- self3acc1.py
+++ self3acc1.py
@@ -1,18 +1,8 @@
 from pyrogram import Client, filters
-import random
+from requests import get
+import pyrogram
+import json
 from time import sleep
-from telethon.sync import TelegramClient
-from telethon.tl.types import InputPeerUser
-from telethon.errors.rpcerrorlist import PeerFloodError
-from telethon.tl.functions.messages import GetDialogsRequest
-from telethon.tl.types import InputPeerEmpty
-from telethon.tl.functions.messages import GetHistoryRequest
-from telethon import TelegramClient, events
-from telethon.errors import SessionPasswordNeededError
-import random
-from time import sleep
-import redis 
-import asyncio 
 
 feri = 'BAAXPuF3nwc9gydVBfwKY_UFrsqGiZINDeCeh6gvKiXQPPq5cQzgq3HtKLsjf7JfIyG5h7POob2zNvIZzVM8STWcx07NtTYZmLvA7hlYEFH5cqBnBp-_7I4ErkGm5_sQZvQEYEwj1NDKwPXBMKQTD4VDXG_cXpaMgCVoDAtPCkeBw_JKJnB0jLMozOuNPSvDon53AACrS7SPunO2ZTSYCxK-NdzrlzeRsKMbkU4xctzhPvCa0y9Lxk_Dg2n6O5_CA6xYvltk-B1jgdo7AXbBMQ88U3xf_bRiPU5JQBf4fBdDtBOzsAM7geOot-JevCKyjn-BH1OVxorxiTMNWA9aLmPDffQeHQA'
 
